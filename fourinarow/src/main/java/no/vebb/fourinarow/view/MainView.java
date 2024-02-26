84f7189ae38c549c26499ed8bd5f992adedf3b06
Vebjorn Oiestad <vebjorn.oiestad@gmail.com>
2024-02-26 23:32:52 +0100

    Hovering brikke

--- fourinarow/src/main/java/no/vebb/fourinarow/view/MainView.java
+++ fourinarow/src/main/java/no/vebb/fourinarow/view/MainView.java
@@ -1,6 +1,7 @@
 package no.vebb.fourinarow.view;
 
 import javafx.event.EventHandler;
+import javafx.event.EventType;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
@@ -12,7 +13,6 @@ import javafx.scene.paint.Color;
 import no.vebb.fourinarow.controller.MainController;
 import no.vebb.fourinarow.model.Cell;
 import no.vebb.fourinarow.model.CellPosition;
-import no.vebb.fourinarow.model.CellType;
 import no.vebb.fourinarow.model.GameState;
 
 public class MainView extends VBox {
@@ -26,6 +26,8 @@ public class MainView extends VBox {
     private Button resetButton;
     private Label stateLabel;
 
+    private Cell previewCell = null;
+
     private final int NUMBER_OF_ROWS;
     private final int NUMBER_OF_COLUMNS;
     private double pieceSize = 80;
@@ -37,10 +39,8 @@ public class MainView extends VBox {
         this.NUMBER_OF_ROWS = rows;
         this.NUMBER_OF_COLUMNS = columns;
         this.model = model;
-        this.boardWidth = (pieceSize + margin) * NUMBER_OF_COLUMNS + margin; // TODO: ikke definer board size to steder
-        this.boardHeight = (pieceSize + margin) * NUMBER_OF_ROWS + margin;
-        this.canvas = new Canvas(boardWidth, boardHeight);
         setBoardSize();
+        this.canvas = new Canvas(boardWidth, boardHeight);
         this.controller = controller;
         this.stateLabel = new Label();
         initializeResetButton();
@@ -50,17 +50,30 @@ public class MainView extends VBox {
     private void initializeCanvas() {
         this.pane = new Pane();
         this.getChildren().addAll(stateLabel, pane);
+        this.pane.getChildren().add(canvas);
 
         EventHandler<MouseEvent> eventHandler = new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent e) {
+                EventType<? extends MouseEvent> eventType = e.getEventType();
                 int x = (int) (e.getSceneX() / boardWidth * NUMBER_OF_COLUMNS);
-                controller.placePiece(x);
+                if (eventType == MouseEvent.MOUSE_CLICKED) {
+                    controller.placePiece(x);
+                }
+                if (model.getGameState() == GameState.IN_GAME && eventType != MouseEvent.MOUSE_EXITED) {
+                    previewCell = new Cell(model.getTurnColor(), new CellPosition(NUMBER_OF_ROWS, x));
+                } else {
+                    previewCell = null;
+                }
                 draw();
             }
         };
-        pane.addEventFilter(MouseEvent.MOUSE_CLICKED, eventHandler);
-        this.pane.getChildren().add(canvas);
+
+        this.pane.setOnMouseClicked(eventHandler);
+        this.pane.setOnMouseMoved(eventHandler);
+        this.pane.setOnMouseExited(eventHandler);
+        this.pane.setOnMouseEntered(eventHandler);
+        this.pane.setOnMouseDragged(eventHandler);
     }
 
     private void initializeResetButton() {
@@ -74,11 +87,14 @@ public class MainView extends VBox {
 
     public void draw() {
         GraphicsContext g = this.canvas.getGraphicsContext2D();
-        g.setFill(Color.BLACK);
+        g.setFill(Color.WHITE);
         g.fillRect(0, 0, this.getWidth(), this.getHeight());
+        g.setFill(Color.BLACK);
+        g.fillRect(0, pieceSize + margin, this.getWidth(), this.getHeight());
         for (Cell cell : model.getBoardCells()) {
             drawCell(cell, g);
         }
+        drawCell(previewCell, g);
         updateLabel();
     }
 
@@ -87,13 +103,6 @@ public class MainView extends VBox {
         StringBuilder text = new StringBuilder();
         switch (gameState) {
             case IN_GAME:
-                text.append("It's ");
-                if (model.getTurnColor() == CellType.BLUE) {
-                    text.append("blue");
-                } else {
-                    text.append("red");
-                }
-                text.append("'s turn");
                 break;
             case TIE:
                 text.append("It's a tie");
@@ -111,6 +120,8 @@ public class MainView extends VBox {
     }
 
     private void drawCell(Cell cell, GraphicsContext g) {
+        if (cell == null)
+            return;
         switch (cell.getColor()) {
             case BLUE:
                 g.setFill(Color.BLUE);
@@ -129,7 +140,7 @@ public class MainView extends VBox {
         int column = cellPos.getColumn();
 
         double x = column * (pieceSize + margin) + margin;
-        double y = (NUMBER_OF_ROWS - row - 1) * (pieceSize + margin) + margin;
+        double y = (NUMBER_OF_ROWS - row) * (pieceSize + margin) + margin;
         g.fillOval(x, y, pieceSize, pieceSize);
     }
 
@@ -138,14 +149,18 @@ public class MainView extends VBox {
         double y = pane.getBoundsInParent().getMinY();
         double width = this.getWidth();
         double height = this.getHeight();
-        pieceSize = Math.min((width - x) / NUMBER_OF_COLUMNS, (height - y) / NUMBER_OF_ROWS);
+        pieceSize = Math.min((width - x) / NUMBER_OF_COLUMNS, (height - y) / NUMBER_OF_ROWS); // TODO: fix scaling
         setBoardSize();
+        setCanvasSize();
         draw();
     }
 
     private void setBoardSize() {
         this.boardWidth = (pieceSize + margin) * NUMBER_OF_COLUMNS + margin;
-        this.boardHeight = (pieceSize + margin) * NUMBER_OF_ROWS + margin;
+        this.boardHeight = (pieceSize + margin) * (NUMBER_OF_ROWS + 1) + margin;
+    }
+
+    private void setCanvasSize() {
         this.canvas.setWidth(boardWidth);
         this.canvas.setHeight(boardHeight);
     }
